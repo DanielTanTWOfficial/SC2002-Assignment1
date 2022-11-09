e1156685cfc281a11761fefe31e3c688dd6c4dc8
czhi-heng <angus2998@hotmail.com>
2022-11-09 17:41:07 +0800

    JavaDoc for Review And ReviewController Class

--- src/model/Review.java
+++ src/model/Review.java
@@ -2,14 +2,33 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * @author zhiheng
+ * The Review class, which implements the Serializable interface 
+ */
+
 public class Review implements Serializable {
 	/**
-	 * 
+	 * Automatically generates a serialVersionUID which is used to verify that the sender 
+	 * and receiver of a serialized object have loaded classes for that object that are 
+	 * compatible with respect to serialization during deserialization. 
 	 */
 	private static final long serialVersionUID = -705589914269932166L;
 	private String reviewerName;
 	private String review;
 	private double rating;
+
+	/**
+	 * Default constructor
+	 */
+	public Review(){}
+
+	/**
+	 * Constructor to create a new Review.
+	 * @param reviewerName
+	 * @param review
+	 * @param rating
+	 */
 	
 	public Review(String reviewerName , String review , double rating)
 	{
@@ -18,31 +37,61 @@ public class Review implements Serializable {
 		this.rating = rating;
 	}
 	
+	
+	/** 
+	 * This method is used to get the review of the movie
+	 * @return String
+	 */
 	public String getReview()
 	{
 		return review;
 	}
 	
+	
+	/** 
+	 * This method is used to change the review of the movie. 
+	 * @param review
+	 */
 	public void setReview(String review)
 	{
 		this.review = review;
 	}
 	
+	
+	/** 
+	 * This method is used to get the Reviewer Name of the Review.
+	 * @return String
+	 */
 	public String getReviewerName()
 	{
 		return reviewerName;
 	}
 	
+	
+	/** 
+	 * This method is used to change the Reviewer Name of the Review. 
+	 * @param reviewerName
+	 */
 	public void setReviewerName(String reviewerName)
 	{
 		this.reviewerName = reviewerName;
 	}
 	
+	
+	/** 
+	 * This method is used to get the rating of the movie from the Review object.
+	 * @return double
+	 */
 	public double getRating()
 	{
 		return rating;
 	}
 	
+	
+	/** 
+	 * This method is used to change the movie rating and it is from a range from 1-5. 
+	 * @param rating
+	 */
 	public void setRating(Double rating)
 	{
 		int correct = 0;
@@ -60,6 +109,9 @@ public class Review implements Serializable {
 
 	}
 	
+	/**
+	 * Prints information about the review being submitted for a movie
+	 */
 	public void printReview() {
 		System.out.println("Reviewed by: " + this.reviewerName);
 		System.out.println("Review: \n" + this.review);
